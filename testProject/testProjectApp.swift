b2fc6f455db5302b6c00bdfffa0efb9a3bc7f08a
paulo <paulomantillaa@gmail.com>
2026-02-17 22:59:29 -0600

    Initial Commit

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Paulo Mantilla on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
